66591310ae5daa8feb3bddd05521afb1fe33d6ee
zhengzhang <zhengzhang@ruijie.com.cn>
2023-08-15 17:43:10 +0800

    CV直线

--- opencv03/src/case2-2.cpp
+++ opencv03/src/case2-2.cpp
@@ -9,18 +9,17 @@ int max_count = 255;
 int threshold_value = 100;
 const char* output_lines = "Hough Lines";
 Mat src, roiImage, dst;
-void detectLines(int, void*);
 void morhpologyLines(int, void*);
 int main(int argc, char** argv) {
-	src = imread("D:/gloomyfish/case2.png", IMREAD_GRAYSCALE);
+	src = imread("G:/project/c/opencv/opencv03/source/case2.png", IMREAD_GRAYSCALE);
 	if (src.empty()) {
 		printf("could not load image...\n");
 		return -1;
 	}
-	namedWindow("input image", CV_WINDOW_AUTOSIZE);
+	namedWindow("input image", WINDOW_AUTOSIZE);
 	imshow("input image", src);
-	namedWindow(output_lines, CV_WINDOW_AUTOSIZE);
-	Rect roi = Rect(10, 10, src.cols - 20, src.rows - 20);
+	namedWindow(output_lines, WINDOW_AUTOSIZE);
+	Rect roi = Rect(10, 10, src.cols - 20, src.rows - 20);//src.cols - 20, src.rows - 20
 	roiImage = src(roi);
 	imshow("ROI image", roiImage);
 	// createTrackbar("threshold:", output_lines, &threshold_value, max_count, detectLines);
@@ -29,19 +28,6 @@ int main(int argc, char** argv) {
 
 	waitKey(0);
 	return 0;
-}
-
-void detectLines(int, void*) {
-	Canny(roiImage, dst, threshold_value, threshold_value * 2, 3, false);
-	//threshold(roiImage, dst, 0, 255, THRESH_BINARY | THRESH_OTSU);
-	vector<Vec4i> lines;
-	HoughLinesP(dst, lines, 1, CV_PI / 180.0, 30, 30.0, 0);
-	cvtColor(dst, dst, COLOR_GRAY2BGR);
-	for (size_t t = 0; t < lines.size(); t++) {
-		Vec4i ln = lines[t];
-		line(dst, Point(ln[0], ln[1]), Point(ln[2], ln[3]), Scalar(0, 0, 255), 2, 8, 0);
-	}
-	imshow(output_lines, dst);
 }
 
 void morhpologyLines(int, void*) {
