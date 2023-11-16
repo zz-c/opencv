e8e50d2f33183a1b88915a889a490df4c5d59d82
zhengzhang <zhengzhang@ruijie.com.cn>
2023-11-16 16:03:45 +0800

    cv08

--- opencv08/src/facerecog_03.cpp
+++ opencv08/src/facerecog_03.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv) {
 	Mat gray, binary;
 	cvtColor(src, gray, COLOR_BGR2GRAY);
 	threshold(gray, binary, 0, 255, THRESH_BINARY | THRESH_OTSU);
-	//imshow("binary image", binary);
+	imshow("binary image", binary);
 
 	vector<Vec4i> hireachy;
 	vector<vector<Point>> contours;
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
 	Mat result = src.clone();
 	for (int i = 0; i < contours.size(); i++) {
 		double area = contourArea(contours[i]);
-		if (area > 1e5 || area < 1e2) continue;
+		if (area > 1e5 || area < 1e2) continue;//10的n次方
 		drawContours(result, contours, i, Scalar(0, 0, 255), 2, 8);
 		double theta = calcPCAOrientation(contours[i], result);
 	}
@@ -35,7 +35,7 @@ int main(int argc, char** argv) {
 	waitKey(0);
 	return 0;
 }
-
+//计算方向
 double calcPCAOrientation(vector<Point> &pts, Mat &image) {
 	int size = static_cast<int>(pts.size());
 	Mat data_pts = Mat(size, 2, CV_64FC1);
@@ -49,10 +49,10 @@ double calcPCAOrientation(vector<Point> &pts, Mat &image) {
 	PCA pca_analysis(data_pts, Mat(), PCA::DATA_AS_ROW);
 	Point cnt = Point(static_cast<int>(pca_analysis.mean.at<double>(0, 0)),
 					static_cast<int>(pca_analysis.mean.at<double>(0, 1)));
-	circle(image, cnt, 2, Scalar(0, 255, 0), 2, 8, 0);
+	circle(image, cnt, 2, Scalar(0, 255, 0), 2, 8, 0);//画中心点
 
-	vector<Point2d> vecs(2);
-	vector<double> vals(2);
+	vector<Point2d> vecs(2);//特征矩阵
+	vector<double> vals(2);//特征向量
 	for (int i = 0; i < 2; i++) {
 		vals[i] = pca_analysis.eigenvalues.at<double>(i, 0);
 		printf("Th %d eigen value : %.2f\n", i, vals[i]);
@@ -65,7 +65,7 @@ double calcPCAOrientation(vector<Point> &pts, Mat &image) {
 	line(image, cnt, p1, Scalar(255, 0, 0), 2, 8, 0);
 	line(image, cnt, p2, Scalar(255, 255, 0), 2, 8, 0);
 
-	double angle = atan2(vecs[0].y, vecs[0].x);
+	double angle = atan2(vecs[0].y, vecs[0].x);//三角函数求角度
 	printf("angle : %.2f\n", 180 * (angle / CV_PI));
 	return angle;
 }
